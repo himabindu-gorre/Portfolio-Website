58862638eb57c00336066c30c851a732970c7d05
himabindu-gorre <himabindugorre7965@gmail.com>
2025-05-16 19:28:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navabar from './Components/Navbar/Navabar'
+import Hero from './Components/Hero/Hero'
+import About from './Components/About/About'
+import Service from './Components/Services/Service'
+import MyWork from './Components/MyWork/MyWork'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Navabar/>
+      <Hero />
+      <About/>
+      <Service />
+      <MyWork />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
+
+// 2:40:22 
